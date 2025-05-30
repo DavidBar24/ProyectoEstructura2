ca067b587f0f7d14920016cc3a4aebc699369d3c
KaKa/ <david.barreto@uao.edu.co>
2025-05-30 18:43:14 -0500

    Cambio info mascota new

--- frontend/src/Contenedores/contenedorInfoMascotas/contenedorInfoMascotas.js
+++ frontend/src/Contenedores/contenedorInfoMascotas/contenedorInfoMascotas.js
@@ -1,6 +1,4 @@
-
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState } from 'react';
 import InfoMascotas from '../../Componentes/infoMascotas/infoMascotas';
 
 const ContenedorInfoMascotas = () => {
